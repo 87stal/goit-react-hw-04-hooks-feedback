0a6f0b7ba487032da8df1c82cd15896824e037b2
87stal <87stal@gmail.com>
2021-01-11 11:09:17 +0300

    Updates

---
+++ precache-manifest.66d66d18dc383656cb7b9e79c39a194f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e850d209f27aa21931f1abd2e46c8c1",
+    "url": "/goit-react-hw-04-hooks-feedback/index.html"
+  },
+  {
+    "revision": "43101594a7e96d3560b2",
+    "url": "/goit-react-hw-04-hooks-feedback/static/css/main.4ceb999c.chunk.css"
+  },
+  {
+    "revision": "eba85ff6eab33b257a0e",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.c2f40012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/2.c2f40012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43101594a7e96d3560b2",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/main.953a7dde.chunk.js"
+  },
+  {
+    "revision": "567fea8629aa48e09379",
+    "url": "/goit-react-hw-04-hooks-feedback/static/js/runtime-main.a620f54e.js"
+  }
+]);
